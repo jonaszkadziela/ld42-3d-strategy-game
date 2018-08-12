3eb4cd29899bf5d0786d1506dfbab819f5eb75fe
Jonasz Kądziela <jonaszkadziela@o2.pl>
2018-08-12 15:15:33 +0200

    Created graphic representation of the path

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	[Range(0, 1)]
+	public int xRotation;
+	[Range(0, 1)]
+	public int yRotation;
+	[Range(0, 1)]
+	public int zRotation;
+	public float rotationSpeed;
+
+	void Update()
+	{
+		this.transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * rotationSpeed * Time.deltaTime, Space.World);
+	}
+}
---
+++ Assets/Scripts/ShowPath.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ShowPath : MonoBehaviour
+{
+	public NavMeshAgent agent;
+	public LineRenderer line;
+	public GameObject targetIndicator;
+
+	public float lineWidth = 0.05f;
+	public Color lineColor;
+
+	void Start()
+	{
+		line.startWidth = lineWidth;
+		line.endWidth = lineWidth;
+		line.startColor = lineColor;
+		line.endColor = lineColor;
+	}
+
+	void Update()
+	{
+		NavMeshPath path = agent.path;
+
+		if (agent.hasPath && agent.remainingDistance > agent.stoppingDistance)
+		{
+			if (!targetIndicator.activeInHierarchy)
+			{
+				targetIndicator.SetActive(true);
+			}
+
+			targetIndicator.transform.position = agent.pathEndPosition;
+
+			line.positionCount = path.corners.Length;
+
+			for (int i = 0; i < line.positionCount; i++)
+			{
+				line.SetPosition(i, path.corners[i]);
+			}
+		}
+		else
+		{
+			line.positionCount = 0;
+			targetIndicator.SetActive(false);
+		}
+	}
+}
